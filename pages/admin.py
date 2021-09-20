611ed338a527216cda89320ec5a1b69f370ecb0d
unknown <shux121995@gmail.com>
2021-09-20 23:32:46 +0300

    person created

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Person
 
-# Register your models here.
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
+    list_per_page = 50
--- pages/views.py
+++ pages/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 
 
 def index(request):
+    # Person.objects.filter(reduce(and_, [Q(name__contains=s) for s in search]))
     return render(request, 'pages/index.html', {})
 
