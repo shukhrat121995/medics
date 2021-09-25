2ad3430f997a1d2f6f1342bef725f8f55a3f0ad1
unknown <shux121995@gmail.com>
2021-09-25 22:15:04 +0300

    filter by date added

--- static/pages/js/main.js
+++ static/pages/js/main.js
@@ -2,17 +2,23 @@ function ShowPersons() {
     query = document.getElementById('search_query').value
     gender = document.getElementById("gender_id");
     gender_selected = gender.options[gender.selectedIndex].value;
+    birth_from_date = document.getElementById('birthFromDate').value
+    birth_to_date = document.getElementById('birthToDate').value
+
     // console.log(gender_selected)
-    // console.log(query)
     $.ajax({
         url: "../getpersons",
         type: "GET",
         data: {
             "query": query,
-            "gender": gender_selected
+            "gender": gender_selected,
+            "birth_from_date": birth_from_date,
+            "birth_to_date": birth_to_date
         },
         success: function (data) {
             $("#persons_list").html(data);
         }
     });
 }
+
+
