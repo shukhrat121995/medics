d517e9e71b726135e244d64dab8af9b036a194b8
unknown <shux121995@gmail.com>
2021-09-28 21:42:16 +0300

    father and mother tables added

--- pages/admin.py
+++ pages/admin.py
@@ -1,9 +1,23 @@
 from django.contrib import admin
-from .models import Person
+from .models import Person, Father, Mother
 
 
 @admin.register(Person)
 class PersonAdmin(admin.ModelAdmin):
-    list_display = ['name', 'gender', 'birth', 'death', 'created_at']
+    list_display = ['name', 'gender', 'birth', 'death', 'language', 'created_at']
+    search_fields = ['name']
+    list_per_page = 50
+
+
+@admin.register(Father)
+class FatherAdmin(admin.ModelAdmin):
+    list_display = ['name', 'occupation']
+    search_fields = ['name']
+    list_per_page = 50
+
+
+@admin.register(Mother)
+class MotherAdmin(admin.ModelAdmin):
+    list_display = ['name', 'maiden_name', 'occupation']
     search_fields = ['name']
     list_per_page = 50
--- pages/models.py
+++ pages/models.py
@@ -1,15 +1,42 @@
 from django.db import models
 
 
+class Father(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    occupation = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Mother(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    maiden_name = models.CharField(max_length=255, blank=True)
+    occupation = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Person(models.Model):
     GENDER = [
         ('Male', 'Male'),
         ('Female', 'Female'),
     ]
-    name = models.CharField(max_length=255)
+    LANGUAGE = [
+        ('Finnish', 'Finnish'),
+        ('Swedish', 'Swedish')
+    ]
+    name = models.CharField(max_length=255, unique=True)
     gender = models.CharField(max_length=6, choices=GENDER, blank=True)
+    place_of_birth = models.CharField(max_length=255, blank=True)
     birth = models.DateField(null=True, blank=True)
     death = models.DateField(null=True, blank=True)
+
+    father = models.ForeignKey(Father, on_delete=models.SET_NULL, blank=True, null=True)
+    mother = models.ForeignKey(Mother, on_delete=models.SET_NULL, blank=True, null=True)
+
+    language = models.CharField(max_length=7, choices=LANGUAGE, blank=True)
 
     created_at = models.DateTimeField(auto_now_add=True, editable=False, blank=True, null=True)
 
