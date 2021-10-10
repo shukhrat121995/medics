af6f97e4271f4b7feb59bd903d383073ce159f59
unknown <shux121995@gmail.com>
2021-10-10 13:54:26 +0300

    date of birth added for children model

--- family/models.py
+++ family/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Father(models.Model):
     class Meta:
         ordering = ['-id']
+
     name = models.CharField(max_length=255, unique=True)
     occupation = models.CharField(max_length=255, blank=True)
 
@@ -14,6 +15,7 @@ class Father(models.Model):
 class Mother(models.Model):
     class Meta:
         ordering = ['-id']
+
     name = models.CharField(max_length=255, unique=True)
     maiden_name = models.CharField(max_length=255, blank=True)
     occupation = models.CharField(max_length=255, blank=True)
@@ -25,6 +27,7 @@ class Mother(models.Model):
 class Spouse(models.Model):
     class Meta:
         ordering = ['-id']
+
     person = models.ForeignKey('pages.Person', on_delete=models.SET_NULL, blank=True, null=True)
     name = models.CharField(max_length=255, unique=True)
     occupation = models.CharField(max_length=255, blank=True)
@@ -36,8 +39,10 @@ class Spouse(models.Model):
 class Children(models.Model):
     class Meta:
         ordering = ['-id']
+
     person = models.ForeignKey('pages.Person', on_delete=models.SET_NULL, blank=True, null=True)
     name = models.CharField(max_length=255)
+    date_of_birth = models.DateField(null=True, blank=True)
 
     def __str__(self):
         return self.name
