69ed23a36c8fea4847f980c11eba28864adc5949
unknown <shux121995@gmail.com>
2021-10-30 21:49:17 +0300

    ordering maticulation and licentiate of medicine added for admin panel

--- pages/admin.py
+++ pages/admin.py
@@ -4,9 +4,23 @@ from .models import Person
 
 @admin.register(Person)
 class PersonAdmin(admin.ModelAdmin):
-    list_display = ['name', 'gender', 'birth', 'death', 'language', 'created_at']
+    @admin.display(ordering='matriculation__date')
+    def matriculation(self, obj):
+        return obj.matriculation.date
+
+    @admin.display(ordering='licentiateofphilosophy__date')
+    def licentiate_of_medicine(self, obj):
+        return obj.licentiateofphilosophy.date
+
+    list_display = ['name',
+                    'gender',
+                    'birth',
+                    'death',
+                    'language',
+                    'matriculation',
+                    'licentiate_of_medicine',
+                    'created_at'
+                    ]
     autocomplete_fields = ['father', 'mother']
     search_fields = ['name']
     list_per_page = 50
-
-
